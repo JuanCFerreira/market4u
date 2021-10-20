82bef0d79d8569fb2e2cec89e10eef2aa4251774
Juan Carlos <juancferreira54@gmail.com>
2021-10-20 05:35:16 -0300

    Configurado ambiente

---
+++ market4u/database/migrations/2021_10_20_081447_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('client');
+            $table->date('date');
+            $table->double('value', 8, 2);
+            $table->string('location');
+            $table->json('products');
+            $table->integer('quantity');
+            $table->double('unitary_value', 8, 2);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
